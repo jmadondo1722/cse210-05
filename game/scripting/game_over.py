bd90f0770d729b85f1ffec17fc1cd2132318abc9
TeigenB <barber.teigen@gmail.com>
2022-11-18 11:42:48 -0700

    finished game over, once Josiah is done with his part we are ready for debugging

--- game/scripting/game_over.py
+++ game/scripting/game_over.py
@@ -1,9 +1,13 @@
 import constants
+import time
 from game.shared.point import Point
 from game.casting.actor import Actor
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.script import Script
 from game.services.keyboard_service import KeyboardService
+from game.casting.player1 import Player1
+from game.casting.player2 import Player2
+
 
 class Game_Over:
     """Game_Over triggers when a player collides with their opponent's trail...
@@ -15,6 +19,9 @@ class Game_Over:
     def __init__(self):
       self.actor = Actor()
       self.controlactors = ControlActorsAction(KeyboardService)
+      self.script = Script()
+      self.player1 = Player1()
+      self.player2 = Player2()
       print("game over init")
 
     def do_game_over(self, cast):
@@ -40,9 +47,25 @@ class Game_Over:
         for cycle in trail:
             cycle.set_color(constants.WHITE)
 
+
+        left = Point(-constants.CELL_SIZE, 0)
+        right = Point(constants.CELL_SIZE, 0)
+        up = Point(0, -constants.CELL_SIZE)
+        down = Point(0, constants.CELL_SIZE)
+        wait = time.sleep(1)
+
+        self.script.add_action("movement", left)
+        self.script.add_action("movement", up)
+        self.script.add_action("movement", right)
+        self.script.add_action("movement", down)
+
+
         while 1:
-            self.controlactors.execute(cast, Script)
-            #i want to send a list of actions to execute and have that go on a loop
-            #i need it to work for both cycles
-            #i think the program wants me to send a list of actions through script
-            #then send that list to execute
+        #a never-ending loop that keeps the characters running
+        #in their own squares
+
+            actions = self.script.get_actions("movement")
+            for action in actions:
+                self.player1.turn_head
+                self.player2.turn_head
+                wait
